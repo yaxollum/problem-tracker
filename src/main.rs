e8d48d3d1d40382621eef5c4c9fda3278232bb30
Peter Ye <yaxollum@gmail.com>
2021-07-12 14:47:54 -0400

    Restructure main function to make it easier to test

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,56 @@ extern crate lalrpop_util;
 
 lalrpop_mod!(pub parser); // synthesized by LALRPOP
 
+use error::InterpreterError;
 use interpreter::Interpreter;
+use report::Report;
 use std::io;
-use std::io::prelude::*;
 
-fn main() {
-    let stdin = io::stdin();
+enum RunResult {
+    Report(Report),
+    ParseError {
+        line_num: u32,
+        line: String,
+    },
+    RuntimeError {
+        line_num: u32,
+        error: InterpreterError,
+    },
+    ReportError(String),
+}
+
+fn run<T: io::BufRead>(input: T) -> RunResult {
     let mut exec = Interpreter::default();
 
-    for (line, line_num) in stdin.lock().lines().zip(1..) {
+    for (line, line_num) in input.lines().zip(1..) {
         let line = line.unwrap();
         if let Ok(cmd) = parser::FullCommandParser::new().parse(&line) {
-            if let Err(msg) = exec.next_command(cmd) {
-                eprintln!("Line {}: {}", line_num, msg);
-                std::process::exit(1);
+            if let Err(error) = exec.next_command(cmd) {
+                return RunResult::RuntimeError { line_num, error };
             }
         } else {
-            eprintln!("Could not parse line {}: \"{}\"", line_num, line);
-            std::process::exit(1);
+            return RunResult::ParseError { line_num, line };
         }
     }
     match exec.report() {
-        Ok(report) => println!("{}", report),
-        Err(msg) => {
+        Ok(report) => RunResult::Report(report),
+        Err(msg) => RunResult::ReportError(msg.to_owned()),
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    match run(stdin.lock()) {
+        RunResult::Report(report) => println!("{}", report),
+        RunResult::ParseError { line_num, line } => {
+            eprintln!("Could not parse line {}: \"{}\"", line_num, line);
+            std::process::exit(1);
+        }
+        RunResult::RuntimeError { line_num, error } => {
+            eprintln!("Line {}: {}", line_num, error);
+            std::process::exit(1);
+        }
+        RunResult::ReportError(msg) => {
             eprintln!("{}", msg);
             std::process::exit(1);
         }
